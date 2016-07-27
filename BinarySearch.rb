0f59571177c3f5f1b88ca1454dd87bb482b9bda9
rashid6 <rashid.parwej@goibibo.com>
2016-07-27 10:58:11 +0530

    First day code for BinarySearch

---
+++ BinarySearch.rb
@@ -0,0 +1,35 @@
+load "HelloWorld.rb"
+class BinarySearch
+
+  def initialize(array, element)
+    @array = array
+    @element = element
+  end
+
+  def find
+    sorted = @array.sort
+    low = 0
+    high = @element.size - 1
+    middle = (low + high) / 2
+    while test = sorted[middle]
+      if test > @element
+        high -= 1
+      elsif test < @element
+        low += 1
+      else
+        return true
+      end
+      middle = (low + high) / 2
+    end
+    false
+  end
+end
+
+@obj = BinarySearch.new([1,2,3,4,5], 9)
+@obj.find
+result = @obj.find
+puts result
+hello = HelloWorld.new("World")
+puts hello
+
+
